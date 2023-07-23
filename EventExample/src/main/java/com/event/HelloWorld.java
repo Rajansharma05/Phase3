e1d5560d737c667e4595ed940063b540b233ac95
Rajansharma05 <rajan.sharma_cs18@gla.ac.in>
2023-07-23 11:07:12 +0530

    Event Example

---
+++ EventExample/src/main/java/com/event/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.event;
+
+public class HelloWorld {
+	private String message;
+
+	
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return message;
+	}
+	
+}
